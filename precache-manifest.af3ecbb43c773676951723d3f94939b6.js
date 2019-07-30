c9e49b095cf7dba227e9b595a61cf247c1476d9e
unknown <mariia>
2019-07-30 14:37:10 +0300

    Updates

---
+++ precache-manifest.af3ecbb43c773676951723d3f94939b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f0fc34efbed3283695d646fa5cf1482",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "b99328d132a1a3645beb",
+    "url": "/react_tabs/static/css/main.a31145ae.chunk.css"
+  },
+  {
+    "revision": "5f4f799c1079a195b591",
+    "url": "/react_tabs/static/js/2.24cd3689.chunk.js"
+  },
+  {
+    "revision": "b99328d132a1a3645beb",
+    "url": "/react_tabs/static/js/main.fed64441.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
